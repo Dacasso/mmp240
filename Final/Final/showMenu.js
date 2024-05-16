0e18ef87066f49246e7a882c8bdf62d1806e6755
Dante <ogletreedee@gmail.com>
2024-05-16 17:11:06 +0000

    Finally

--- Final/Final/showMenu.js
+++ Final/Final/showMenu.js
@@ -1,5 +1,5 @@
 function showMenu(){
-    var  menu = document.getElementById("topmenu");
+    var  menu = document.getElementById("menu");
     if  (menu.style.display==="none" || menu.style.display === ''){
         menu.style.display="block";
     } else {
